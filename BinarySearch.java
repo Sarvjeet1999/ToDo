62122082464bc0b16deaff829c0d3bae2f56a272
Sarvjeet Singh <32335984+Sarvjeet1999@users.noreply.github.com>
2021-02-17 11:18:32 +0530

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,2 +1,21 @@
+// Binary Search Methods
 
+// METHOD-1:
+// s=0,e=n-1;
+public static void function1(int n,int arr[],int s,int e)
+{
+  if(s>=e) return;
+  int mid = s+(e-s)/2;
+  function1(n,arr,s,mid);
+  function1(n,arr,mid+1,e);
+}
 
+// METHOD-2:
+// s=0,e=n-1;
+public static void function2(int n,int arr[],int s,int e)
+{
+  if(s>=e) return;
+  int mid = s+(e-s+1)/2;
+  function1(n,arr,s,mid-1);
+  function1(n,arr,mid,e);
+}
